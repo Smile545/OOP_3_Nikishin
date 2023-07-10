8131a51db08c2ca54f21e91dab2ece98ab18d692
Bitrix24 3 <dialb3@dialweb.local>
2023-07-10 15:36:20 +0300

    Good

---
+++ heroes/BaseHeroInterface.java
@@ -0,0 +1,24 @@
+package heroes;
+
+
+public interface BaseHeroInterface {
+
+    public default void step() {
+        System.out.println( getName() + " прощел еще немного... " );
+    }
+
+    public default void step( String direction ) {
+        System.out.println( getName() + " прощел " + direction + " еще немного... " );
+    }
+    public default void stepUp()    { this.step( "вперед" ); }
+    public default void stepDown()  { this.step( "назад" ); }
+    public default void stepLeft()  { this.step( "влево" ); }
+    public default void stepRight() { this.step( "вправо" ); }
+
+
+    public default String getInfo() {
+        return getClass().getName();
+    }
+
+    public default String getName()      { return ""; }
+}
---
+++ heroes/HeroInterface.java
@@ -0,0 +1,17 @@
+package heroes;
+
+
+public interface HeroInterface {
+    public static int getHealth() { return 0; }
+    public static void setHealth( int health ) { }
+    public static void setState( int state ) { }
+
+    // Получаем урон
+    public static void doDamage( int damage ) {
+        setHealth( getHealth() - damage );
+        if ( getHealth() <= 0 ) {
+            setHealth( 0 );
+            setState ( -1 );
+        }
+    }
+}
